d8d48f5b68e87330314706117b532e9b0aeef560
Shanu Rane <155867480+shanurane@users.noreply.github.com>
2024-09-13 13:38:17 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.MONGO_URI}/chai`, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/chai`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
